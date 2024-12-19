6af504c7f8d29dbf17d29c86e8eead5f92ff86cc
Stanislav Mayatsky <stasm226@gmail.com>
2024-12-19 13:02:25 +0500

    implemented templates

--- catalog/views.py
+++ catalog/views.py
@@ -4,4 +4,10 @@
 
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_page(request):
+    return render(request, "home.html")
+
+
+def contacts_page(request):
+    return render(request, "contacts.html")
